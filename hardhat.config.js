549c8add68fb4bcbdbfacfa800b705ae0dfafd05
Kemal Demirel <80597081+kemaldemirel1999@users.noreply.github.com>
2022-06-12 12:08:31 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,8 +17,8 @@ module.exports = {
       chainId: 1337,
     },
     rinkeby: {
-      url : "https://eth-rinkeby.alchemyapi.io/v2/1GzIw_dSAF7NbAsjcmJNZbRtRta09p0R",
-      accounts : ["81ccda2dabb1596898615c8c8fba12e098f8c96dd1049a0e01418bc70540fb00"]
+      url : "XXXXXXXXXX",
+      accounts : ["XXXXXXXXXXX"]
     },
   }
 };
